9ce439c38b0e9762044b1fb97628ddc953d7ee7e
Kelly Raila <kelly.raila@gmail.com>
2015-07-13 12:46:05 -0400

    add about page and busidness_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'homes#index'
 
+  resources :businesses, only: [:index, :show]
+
   devise_for :users, controllers: { registrations: :registrations }
   resources :users, only: :show
 end
