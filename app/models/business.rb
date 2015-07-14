1b709393cf47648ac7e636d5e759acc0ca69ae2f
Kelly Raila <kelly.raila@gmail.com>
2015-07-14 15:12:36 -0400

    did stuff with models

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,9 @@
+require 'uri'
+
 class Business < ActiveRecord::Base
   validates :name, presence: true
   validates :address, presence: true
-  validates :summay, presence: true, length: { in: 50..250 }
+  validates :summary, presence: true, length: { in: 25..250 }
   validates :description, length: { maximum: 1000 }, allow_nil: true
+  validates :website, format: { with: URI::regexp(%w(http https)) }, allow_nil: true
 end
---
+++ spec/models/business_spec.rb
@@ -0,0 +1,87 @@
+# require 'rails_helper'
+#
+# RSpec.describe Business, type: :model do
+#   context "a new business" do
+#     let (cafe) {
+#       {
+#         name: "A Cool Cafe",
+#         address: "1234 Street Ave. Boston, MA",
+#         summary: "A small cafe with a variety of snacks."
+#       }
+#     }
+#
+#     let (description) {
+#       "Cupcake ipsum dolor. Sit amet sesame snaps
+#       jujubes. Cheesecake jelly-o croissant tart chocolate bar.
+#       Lollipop cake tart gingerbread cake. Jujubes jujubes topping
+#       tootsie roll macaroon topping chocolate cake. Sweet roll muffin
+#       tootsie roll tiramisu muffin. Candy canes liquorice danish
+#       cheesecake pudding cake sweet. Cookie danish pastry. Caramels
+#       biscuit pie chocolate. Bonbon halvah cake donut cookie caramels
+#       carrot cake liquorice. Brownie jelly-o cupcake brownie topping
+#       gummies halvah. Biscuit donut biscuit soufflé.
+#       Cake macaroon pudding lollipop."
+#     }
+#
+#     it "is valid if only required information is provided" do
+#       business = Business.new(
+#         name: cafe.name,
+#         address: cafe.address,
+#         summary: cafe.summary
+#       )
+#       expect(business.valid?).to eq true
+#     end
+#
+#     it "is valid if optional information is also provided" do
+#       business = Business.new(
+#         name: cafe.name,
+#         address: cafe.address,
+#         summary: cafe.summary,
+#         description: description,
+#         website: "http://www.a-cool-cafe.com",
+#         phone_number: "617-123-4567"
+#         )
+#       expect(business.valid?).to eq true
+#     end
+#
+#     it "is not valid if information is missing" do
+#       business = Business.new(
+#         name: cafe.name,
+#         )
+#       expect(business.valid?).to eq false
+#     end
+#     #
+#     # it "is not valid if the url is not proper format" do
+#     #   cafe = Business.new(
+#     #     display_name: "cafe",
+#     #     password: "password"
+#     #     )
+#     #   expect(cafe.valid?).to eq false
+#     # end
+#     #
+#     # it "is not valid if the summary is wrong length" do
+#         # it "is too long" do
+#         #
+#         # end
+#         #
+#         # it "is too short" do
+#         #
+#         # end
+#     #   cafe = Business.new(
+#     #     display_name: "cafe",
+#     #     password: "password"
+#     #     )
+#     #   expect(cafe.valid?).to eq false
+#     # end
+#
+#     it "is not valid if the description is too long" do
+#       business = Business.new(
+#         name: cafe.name,
+#         address: cafe.address,
+#         summary: cafe.summary,
+#         description: description * 2
+#       )
+#       expect(business.valid?).to eq false
+#     end
+#   end
+# end
