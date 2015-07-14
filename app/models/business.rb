d5dacf930c69e7185c612d524e603718a8a30050
Kelly Raila <kelly.raila@gmail.com>
2015-07-14 15:26:01 -0400

    add full business spec

--- app/models/business.rb
+++ app/models/business.rb
@@ -5,5 +5,6 @@ class Business < ActiveRecord::Base
   validates :address, presence: true
   validates :summary, presence: true, length: { in: 25..250 }
   validates :description, length: { maximum: 1000 }, allow_nil: true
-  validates :website, format: { with: URI::regexp(%w(http https)) }, allow_nil: true
+  validates :website, format: { with: URI::regexp(%w(http https)) },
+    allow_nil: true
 end
