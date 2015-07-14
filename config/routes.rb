7bb4a0ba56c600400bb0b442cdb34c48467a9749
Kelly Raila <kelly.raila@gmail.com>
2015-07-14 19:48:03 -0400

    create form partial for businesses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'businesses#index'
 
-  resources :businesses, only: [:index, :show]
+  resources :businesses, only: [:index, :show, :new, :create]
 
   devise_for :users, controllers: { registrations: :registrations }
   resources :users, only: :show
