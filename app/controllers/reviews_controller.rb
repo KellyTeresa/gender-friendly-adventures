9183c7215dd33e7b98ef74c48e3b8e9d76a6eab0
Kelly Raila <kelly.raila@gmail.com>
2015-07-16 15:02:32 -0400

    update reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,5 +6,38 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @business = Business.find(params[:business_id])
+    @review = Review.new(review_params)
+    @review.business = @business
+    @review.user = current_user
+
+    if @review.save
+      flash[:success] = "Review added!"
+      redirect_to business_path(@business)
+    else
+      announce_errors(@review)
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :terminology,
+      :bathrooms,
+      :gv_staff,
+      :overall,
+      :comment
+    )
+  end
+
+  def announce_errors(review)
+    count = review.errors.count
+    flash[:alert] = %(
+      #{count} #{'error'.pluralize(count)}
+      prohibited this review from being saved:
+      #{review.errors.full_messages.join('. ')}
+    )
   end
 end
