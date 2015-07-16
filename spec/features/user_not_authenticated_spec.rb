648a2689a566a9dba9190429d350f844f00bc72f
Kelly Raila <kelly.raila@gmail.com>
2015-07-16 11:58:17 -0400

    hound fix

--- spec/features/user_not_authenticated_spec.rb
+++ spec/features/user_not_authenticated_spec.rb
@@ -3,7 +3,8 @@ require "rails_helper"
 feature "a user who is not signed in cannot access member-only pages" do
   scenario "user attempts to add new business" do
     visit new_business_path
-    expect(page).to have_content "You need to sign in or sign up before continuing."
+    expect(page).to have_content "You need to sign in
+      or sign up before continuing."
   end
 
   scenario "user cannot see link to profile" do
@@ -16,12 +17,14 @@ feature "a user who is not signed in cannot access member-only pages" do
     scenario "from business show page" do
       visit business_path(venue)
       click_link "Add Review"
-      expect(page).to have_content "You need to sign in or sign up before continuing."
+      expect(page).to have_content "You need to sign in
+        or sign up before continuing."
     end
 
     scenario "from url" do
       visit new_business_review_path(venue)
-      expect(page).to have_content "You need to sign in or sign up before continuing."
+      expect(page).to have_content "You need to sign in
+        or sign up before continuing."
     end
   end
 end
