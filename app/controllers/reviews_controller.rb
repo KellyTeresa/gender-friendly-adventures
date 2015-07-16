29ca9fcab57e03b78734daf1209c91e392d5e86a
Kelly Raila <kelly.raila@gmail.com>
2015-07-16 14:53:24 -0400

    set up review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,8 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def new
+    @business = Business.find(params[:business_id])
+    @review = Review.new
   end
 
   def create
