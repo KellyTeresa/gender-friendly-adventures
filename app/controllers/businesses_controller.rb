98d2648f579c87b1535742aa6cd9f81344c323ef
Kelly Raila <kelly.raila@gmail.com>
2015-07-16 11:13:20 -0400

    add authentication

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,4 +1,6 @@
 class BusinessesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @businesses = Business.all
   end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,11 @@
+class ReviewsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def new
+
+  end
+
+  def create
+
+  end
+end
