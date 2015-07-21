a2382c0f508a5ff0102d3d83ad6bd85317efd587
Kelly Raila <kelly.raila@gmail.com>
2015-07-21 19:08:18 -0400

    set up basic admin dashboard page with spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admin do
-  get 'dashboard/index'
-  end
-
   root "venues#index"
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
   namespace :admin do
     resources :users, :venues
+    get '/dashboard' => 'dashboard#index'
   end
   resources :venues, only: [:show, :index, :create, :new] do
     resources :reviews, only: [:new, :create]
--- spec/features/admin_accesses_dashboard_spec.rb
+++ spec/features/admin_accesses_dashboard_spec.rb
@@ -3,11 +3,12 @@ require "rails_helper"
 feature "admin user can access admin dashboard" do
   context "signed-in admin interacts with dashboard" do
     let(:admin) { FactoryGirl.create(:user, admin: true) }
-    xscenario "navigating to dashboard from url" do
+    scenario "navigating to dashboard from url" do
       visit root_path
       sign_in_as(admin)
       visit admin_dashboard_path
-      expect(page).to have_content "Admin Dashboard"
+      expect(page).to have_content "View Users"
+      expect(page).to have_content "Pending Venues"
     end
 
     scenario "navigating to dashboard from link" do
@@ -37,22 +38,36 @@ feature "admin user can access admin dashboard" do
   end
 
   context "signed-in non-admin does not reach dashboard" do
-    xscenario "navigating to dashboard from url" do
-
+    let(:user) { FactoryGirl.create(:user) }
+    scenario "navigating to dashboard from url" do
+      visit root_path
+      sign_in_as(user)
+      visit admin_dashboard_path
+      expect(page).to_not have_content "View Users"
+      expect(page).to_not have_content "Pending Venues"
+      expect(page).to have_content "You must be signed in as
+        an admin to view this page."
     end
 
-    xscenario "navigating to dashboard from site" do
-
+    scenario "navigating to dashboard from site" do
+      visit root_path
+      sign_in_as(user)
+      expect(page).to_not have_content "Admin Dashboard"
     end
   end
 
   context "signed-out user does not reach dashboard" do
-    xscenario "navigating to dashboard from url" do
-
+    scenario "navigating to dashboard from url" do
+      visit admin_dashboard_path
+      expect(page).to_not have_content "View Users"
+      expect(page).to_not have_content "Pending Venues"
+      expect(page).to have_content "You must be signed in as
+        an admin to view this page."
     end
 
-    xscenario "navigating to dashboard from site" do
-
+    scenario "navigating to dashboard from site" do
+      visit root_path
+      expect(page).to_not have_content "Admin Dashboard"
     end
   end
 end
