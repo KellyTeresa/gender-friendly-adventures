9838815782d505b7f7bc9334f9d2d3bbc917a518
Kelly Raila <kelly.raila@gmail.com>
2015-07-22 16:25:51 -0400

    continue building admin features and specs

---
+++ spec/features/admin_edits_venue_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+feature "admit edits a venue", %(
+As an admin, I want to edit a venue
+So I can correct information before approving it
+Or update information that has changed.
+) do
+  xscenario "from admin panel" do
+
+  end
+
+  xscenario "from regular show page" do
+
+  end
+end
