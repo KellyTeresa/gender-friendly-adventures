47813bbc350d3de4b1a16d346e8f954c569a67a5
Kelly Raila <kelly.raila@gmail.com>
2015-07-22 10:59:20 -0400

    fix per hound

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root "venues#index"
-  get '/about' => 'static_pages#about'
-  get '/contact' => 'static_pages#contact'
+  get "/about" => "static_pages#about"
+  # get "/contact" => "static_pages#contact"
   namespace :admin do
     resources :users, :venues
-    get '/dashboard' => 'dashboard#index'
+    get "/dashboard" => "dashboard#index"
   end
   resources :venues, only: [:show, :index, :create, :new] do
     resources :reviews, only: [:new, :create]
