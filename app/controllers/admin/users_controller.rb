ea302666b7f3316a6fc76b01c9668f46f5ade2e7
Kelly Raila <kelly.raila@gmail.com>
2015-07-23 13:07:15 -0400

    extract admin_update to method

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,14 +7,22 @@ class Admin::UsersController < AdminController
     @user = User.find(params[:id])
     if @user == current_user
       flash[:alert] = "You cannot change your own admin status."
-    elsif params[:status] == "add"
-      @user.admin = true
+    else
+      admin_update(@user, params[:status])
+    end
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def admin_update(user, status)
+    if status == "add"
+      user.admin = true
       flash[:success] = "Admin added."
-    elsif params[:status] == "remove"
-      @user.admin = false
+    elsif status == "remove"
+      user.admin = false
       flash[:success] = "Admin removed."
     end
-    @user.save
-    redirect_to admin_users_path
+    user.save
   end
 end
