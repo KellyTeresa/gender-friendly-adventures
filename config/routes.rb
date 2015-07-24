8e26aad1584173ec20d073d8a40aad8b455654f7
Kelly Raila <kelly.raila@gmail.com>
2015-07-24 13:29:12 -0400

    add coming soon placeholder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "venues#index"
   get "/about" => "static_pages#about"
+  get "/coming_soon" => "static_pages#coming_soon"
   # get "/contact" => "static_pages#contact"
   namespace :admin do
     resources :users, only: [:index, :show, :edit, :update]
