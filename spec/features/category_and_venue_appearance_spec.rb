e4822d5319fb7eb9d7d240370c1dd1d5cb208ab4
Kelly Raila <kelly.raila@gmail.com>
2015-08-01 19:59:37 -0400

    new specs

---
+++ spec/features/category_and_venue_appearance_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "Venues appearing in search results" do
+  xscenario "an approved venue will appear" do
+  end
+
+  xscenario "a queued venue will not appear" do
+  end
+end
+
+feature "Categories shown on index page" do
+  xscenario "a category with approved venues will appear" do
+  end
+
+  xscenario "a category with only queued venues will not appear" do
+  end
+end
